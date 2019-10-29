98f3eb9b68f1becbb23475e82c931bebb453455b
jyoung8607 <jyoung8607@gmail.com>
2019-10-29 16:54:11 -0400

    Fixes and clarity for bus checks on TX.

--- panda/board/safety/safety_volkswagen.h
+++ panda/board/safety/safety_volkswagen.h
@@ -62,7 +62,7 @@ static int volkswagen_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   int tx = 1;
 
   // Safety check for HCA_01 Heading Control Assist torque.
-  if (addr == MSG_HCA_01) {
+  if ((bus == 0) && (addr == MSG_HCA_01)) {
     bool violation = false;
 
     int desired_torque = GET_BYTE(to_send, 2) | ((GET_BYTE(to_send, 3) & 0x3F) << 8);
@@ -114,7 +114,7 @@ static int volkswagen_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 
   // FORCE CANCEL: ensuring that only the cancel button press is sent when controls are off.
   // This avoids unintended engagements while still allowing resume spam.
-  if ((addr == MSG_GRA_ACC_01) && !controls_allowed && (bus == 2)) {
+  if ((bus == 2) && (addr == MSG_GRA_ACC_01) && !controls_allowed) {
     // disallow resume and set: bits 16 and 19
     if ((GET_BYTE(to_send, 2) & 0x9) != 0) {
       tx = 0;
