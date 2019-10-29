2eec9ca6dde1db95eaaabe202b4b917848158d8b
jyoung8607 <jyoung8607@gmail.com>
2019-10-29 11:45:54 -0400

    Checkpoint on carcontroller refactoring.

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -53,74 +53,83 @@ class CarController():
     canbus = self.canbus
 
     #
-    # Prepare HCA_01 steering torque message
+    # Prepare HCA_01 Heading Control Assist messages with steering torque.
     #
+    # The factory camera sends at 50Hz while steering and 1Hz when not. When
+    # OP is active, Panda filters HCA_01 from the factory camera and OP emits
+    # HCA_01 at 50Hz. Rate switching creates some confusion in Cabana and
+    # doesn't seem to add value at this time. The rack will accept HCA_01 at
+    # 100Hz if we want to control at finer resolution in the future.
+    #
+
     if frame % P.HCA_STEP == 0:
-      # We send HCA_01 at full rate of 50Hz. The factory camera sends at 50Hz
-      # while steering and 1Hz when not. Rate-switching creates some confusion
-      # in Cabana and doesn't seem to add value, so we send at 50Hz all the
-      # time. The rack does accept HCA at 100Hz if we want to control at finer
-      # resolution in the future.
 
-      # FAULT CONDITION: HCA may not be enabled at standstill. Also stop
+      # FAULT AVOIDANCE: HCA may not be enabled at standstill. Also stop
       # commanding HCA if there's a fault, so the steering rack recovers.
-      if not enabled or (CS.standstill or CS.steeringFault):
-        # Disable Heading Control Assist
-        hca_enabled = False
-        apply_steer = 0
+      if enabled and not (CS.standstill or CS.steeringFault):
 
-      else:
-        # FAULT CONDITION: Requested HCA torque may not exceeds 3.0nm. This is
+        # FAULT AVOIDANCE: Requested HCA torque may not exceed 3.0nm. This is
         # inherently handled by scaling to STEER_MAX. The rack doesn't seem
         # to care about up/down rate, but we have some evidence it may do its
         # own rate limiting, and matching OP helps for accurate tuning.
         apply_steer = int(round(actuators.steer * P.STEER_MAX))
         apply_steer = apply_std_steer_torque_limits(apply_steer, self.apply_steer_last, CS.steeringTorque, P)
 
-        # FAULT CONDITION: HCA may not be enabled for > 360 seconds. Sending
-        # the HCA disabled flag for one frame is sufficient to work around it.
         if apply_steer == 0:
+          # FAULT AVOIDANCE: HCA may not be enabled for > 360 seconds. Setting
+          # the HCA disabled flag for one frame is an effective workaround.
           # We can do this very frequently by disabling HCA when apply_steer
-          # happens to be exactly zero, which naturally happens quite often
-          # during direction changes. This could be expanded with a small
-          # dead-zone to capture more zero crossings, but not seeing a need.
+          # happens to be exactly zero, which happens naturally during a
+          # subset of direction changes. This could be expanded with a small
+          # dead-zone to capture all zero crossings, but not seeing a need.
           hca_enabled = False
           self.hca_enabled_cnt = 0
         else:
           self.hca_enabled_cnt += 1
-          if self.hca_enabled_cnt >=  118 * 100:  # 118s ~ about 2 minutes
-            # The Kansas Crosswind Problem: what happens if we do need to steer
-            # left for six minutes? We have to disable HCA for a frame anyway.
-            # Do so three times within that six minutes to make certain the
-            # timer is reset even if a couple messages are lost.
+          if self.hca_enabled_cnt >=  118 * (100 / HCA_STEP):  # 118s
+            # The Kansas I-70 Crosswind Problem: if we truly do need to steer
+            # in one direction for > 360 seconds, we have to disable HCA for a
+            # frame while actively steering. Testing shows we can just set the
+            # disabled flag, and keep sending a torque value, which keeps the
+            # Panda torque rate limiting safety happy. Do so 3x within the 360
+            # second window for safety.
             hca_enabled = False
             self.hca_enabled_cnt = 0
           else:
             hca_enabled = True
-            # FAULT CONDITION: HCA torque may not be static for > 6 seconds.
+            # FAULT AVOIDANCE: HCA torque may not be static for > 6 seconds.
             # This is intended to detect the sending camera being stuck or
-            # frozen. OP can trip this on a curve when it wants => STEER_MAX.
-            # Avoid this by reducing torque 0.01nm for one frame if it's been
-            # level for more than 1.9 seconds. That makes sure we've sent at
-            # least three trimmed messages within the 6 second span, resetting
-            # the rack timer even if a couple messages are lost.
+            # frozen. OP can trip this on a curve if steering is saturated.
+            # Avoid this by reducing torque 0.01nm for one frame. Do so 3x
+            # within the 6 second period for safety.
             if self.apply_steer_last == apply_steer:
               self.same_torque_cnt += 1
-              if self.same_torque_cnt > 190:  # 1.9s
+              if self.same_torque_cnt > 1.9 * (100 / HCA_STEP):  # 1.9s
                 apply_steer -= (1, -1)[apply_steer < 0]
                 self.same_torque_cnt = 0
             else:
               self.same_torque_cnt = 0
 
+      else:
+        # Continue sending HCA_01 messages, with the enable flags turned off.
+        hca_enabled = False
+        apply_steer = 0
+
+
       self.apply_steer_last = apply_steer
 
-      self.hca_msg_counter = (self.hca_msg_counter + 1) % 16
+      idx = (frame / P.HCA_STEP) % 16
       can_sends.append(volkswagencan.create_mqb_steering_control(self.packer_gw, canbus.gateway, apply_steer,
-                                                                 self.hca_msg_counter, hca_enabled))
+                                                                 idx, hca_enabled))
 
     #
-    # Prepare LDW_02 HUD message with lane lines and confidence levels
+    # Prepare LDW_02 HUD messages with lane borders, confidence levels, and
+    # the LKAS status LED.
     #
+    # The factory camera emits this message at 10Hz. When OP is active, Panda
+    # filters LDW_02 from the factory camera and OP emits LDW_02 at 10Hz.
+    #
+
     if frame % P.LDW_STEP == 0:
       if enabled and not CS.standstill:
         hca_enabled = True
@@ -156,10 +165,28 @@ class CarController():
       self.gra_acc_ondemand_trigger = True
       self.buttonStatesToSend["resumeCruise"] = True
 
-    # In order to support integration at the camera, we can't depend on Panda
-    # to filter the car's message and insert our own. Instead, we wait for the
-    # car's message counter to change, and then send 16 of our own on the bus
-    # exactly one counter increment ahead of the car's next 16 messages.
+    # Prepare GRA_ACC_01 cruise control button message. The car sends this
+    # message at 33hz. OP sends it on-demand only for virtual button presses.
+    #
+    # OP/Panda can see this message but can't filter it when integrated at the
+    # J242 LKAS camera. It could so so if integrated at the J533 gateway, but
+    # we need a generalized solution that works for either.
+    #
+    # The message is counter protected, so we need to time our transmissions
+    # very precisely to achieve fast and fault-free switching between the set
+    # of messages the R242 ACC radar is willing to listen to.
+    #
+    # CAR @ 33Hz: 0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F  0  1  2  3  4  5  6 ...
+    # EON @ 33Hz:           4  5  6  7  8  9  A  B  C  D  E  F  0  1  2  3  GG
+    #
+    # If OP needs to send a button press, it waits to see a GRA_ACC_01 message
+    # counter change, and then immediately follows up with the next increment.
+    # It plays out another 16 (an arbitrary number, about half a second) in
+    # lockstep with each new message from the car. Because the OP's message
+    # counter is synced to the car, R242 pays attention to us immediately. The
+    # messages from the car get discarded as duplicates. When OP stops, the
+    # gap to the next car message is less than 2 * GRA_ACC_STEP and R242 seems
+    # to tolerate this just fine.
 
     # FIXME: This is ugly and I almost hope it doesn't work.
 
