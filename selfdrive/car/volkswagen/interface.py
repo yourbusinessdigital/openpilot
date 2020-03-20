5a891280d3123fb1958cd14df14d6b553b860bf9
Jason Young <jyoung8607@gmail.com>
2020-03-20 16:17:20 -0400

    Enable traditional fingerprinting for PQ

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -1,7 +1,7 @@
 from cereal import car
 from selfdrive.swaglog import cloudlog
 from selfdrive.controls.lib.drive_helpers import create_event, EventTypes as ET
-from selfdrive.car.volkswagen.values import CAR, BUTTON_STATES, NWL, TRANS, GEAR
+from selfdrive.car.volkswagen.values import CAR, BUTTON_STATES, NWL, TRANS, GEAR, MQB_CARS, PQ_CARS
 from common.params import Params
 from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint
 from selfdrive.car.interfaces import CarInterfaceBase
@@ -24,76 +24,81 @@ class CarInterface(CarInterfaceBase):
   def get_params(candidate, fingerprint=gen_empty_fingerprint(), has_relay=False, car_fw=[]):
     ret = CarInterfaceBase.get_std_params(candidate, fingerprint, has_relay)
 
-    # Set common default parameters
+    # Set global default parameters
     ret.radarOffCan = True
     ret.enableCamera = True  # Stock camera detection doesn't apply to VW
+    ret.steerRateCost = 1.0
+    ret.steerActuatorDelay = 0.05  # Hopefully all racks are similar here
+    ret.steerLimitTimer = 0.4
 
-    if candidate == CAR.GENERICMQB:
+    # Override these per-car as necessary
+    ret.lateralTuning.pid.kpBP = [0.]
+    ret.lateralTuning.pid.kiBP = [0.]
+    ret.lateralTuning.pid.kpV = [0.6]
+    ret.lateralTuning.pid.kiV = [0.2]
+    ret.lateralTuning.pid.kf = 0.00006
+
+    # PER-PLATFORM PARAMETERS - DO NOT EDIT HERE TO TUNE INDIVIDUAL VEHICLES
+
+    if candidate in MQB_CARS:
+      # Configurations shared between all MQB vehicles
       ret.carName = "volkswagen"
       ret.safetyModel = car.CarParams.SafetyModel.volkswagen
 
-      # FIXME: Defaulting to VW Golf Mk7 as a baseline.
-      ret.mass = 1500 + STD_CARGO_KG
-      ret.wheelbase = 2.64
-
-      # Determine installed network location by finding the ACC_06 radar message
-      if 0x122 in fingerprint[0]:
-        ret.networkLocation = NWL.fwdCamera
-      else:
-        ret.networkLocation = NWL.gateway
-
-      # Determine transmission type by CAN message(s) present on the bus
-      if 0xAD in fingerprint[0]:
-        # Getriebe_11 message detected: traditional automatic or DSG gearbox
+      # Determine installed network location and trans type from fingerprint
+      ret.networkLocation = NWL.fwdCamera if 0x122 in fingerprint[0] else NWL.gateway
+      if 0xAD in fingerprint[0]:  # Getriebe_11
         ret.transmissionType = TRANS.automatic
-      elif 0x187 in fingerprint[0]:
-        # EV_Gearshift message detected: e-Golf or similar direct-drive electric
+      elif 0x187 in fingerprint[0]:  # EV_Gearshift
         ret.transmissionType = TRANS.direct
-      else:
-        # No trans message at all, must be a true stick-shift manual
+      else:  # No trans at all
         ret.transmissionType = TRANS.manual
 
-    elif candidate == CAR.GENERICPQ:
+    elif candidate in PQ_CARS:
+      # Configurations shared between all PQ35/PQ46/NMS vehicles
       ret.carName = "volkswagen"
       ret.safetyModel = car.CarParams.SafetyModel.volkswagenPq
 
-      # FIXME: Defaulting to VW Golf Mk5 as a baseline.
-      # Until that time, defaulting to VW Golf Mk5 as a baseline.
-      ret.mass = 1500 + STD_CARGO_KG
-      ret.wheelbase = 2.58
-
-      # Determine installed network location by finding the ACC_System radar message
-      if 0x368 in fingerprint[0]:
-        ret.networkLocation = NWL.fwdCamera
-      else:
-        ret.networkLocation = NWL.gateway
-
-      # Determine transmission type by CAN message(s) present on the bus
-      if 0x440 in fingerprint[0]:
-        # Getriebe_1 message detected: traditional automatic or DSG gearbox
+      # Determine installed network location and trans type from fingerprint
+      ret.networkLocation = NWL.fwdCamera if 0x368 in fingerprint[0] else NWL.gateway
+      if 0x440 in fingerprint[0]:  # Getriebe_1
         ret.transmissionType = TRANS.automatic
-      else:
-        # No trans message at all, must be a true stick-shift manual
+      else:  # No trans at all
         ret.transmissionType = TRANS.manual
 
     cloudlog.warning("Detected network location: %s", ret.networkLocation)
     cloudlog.warning("Detected transmission type: %s", ret.transmissionType)
 
-    # FIXME: Per-vehicle parameters need to be reintegrated.
-    # Until then, use some reasonable defaults.
-    tire_stiffness_factor = 1.0
-    ret.centerToFront = ret.wheelbase * 0.45
+    # PER-VEHICLE PARAMETERS - EDIT HERE TO TUNE INDIVIDUAL VEHICLES
 
-    ret.steerRatio = 15.6
-    ret.steerRateCost = 1.0
-    ret.steerActuatorDelay = 0.05  # Hopefully all MQB racks are similar here
-    ret.steerLimitTimer = 0.4
+    if candidate == CAR.GENERICMQB:
+      # FIXME: Defaulting to VW Golf Mk7 as a baseline.
+      ret.mass = 1500 + STD_CARGO_KG  # Average, varies on trim/package
+      ret.wheelbase = 2.64
+      ret.centerToFront = ret.wheelbase * 0.45  # Estimated
+      ret.steerRatio = 15.6
+      tire_stiffness_factor = 1.0
 
-    ret.lateralTuning.pid.kf = 0.00006
-    ret.lateralTuning.pid.kpBP = [0.]
-    ret.lateralTuning.pid.kiBP = [0.]
-    ret.lateralTuning.pid.kpV = [0.6]
-    ret.lateralTuning.pid.kiV = [0.2]
+    elif candidate == CAR.VW_GOLF_MK6:
+      ret.mass = 1375 + STD_CARGO_KG  # Average, varies on trim/package
+      ret.wheelbase = 2.58
+      ret.centerToFront = ret.wheelbase * 0.45  # Estimated
+      ret.steerRatio = 15.6
+      tire_stiffness_factor = 1.0
+
+    elif candidate == CAR.VW_PASSAT_B6:
+      ret.mass = 1540 + STD_CARGO_KG  # Average, varies on trim/package
+      ret.wheelbase = 2.71
+      ret.centerToFront = ret.wheelbase * 0.45  # Estimated
+      ret.steerRatio = 15.6
+      tire_stiffness_factor = 1.0
+
+    elif candidate == CAR.VW_SCIROCCO_MK3:
+      ret.mass = 1375 + STD_CARGO_KG  # Average, varies on trim/package
+      ret.wheelbase = 2.58
+      ret.centerToFront = ret.wheelbase * 0.45  # Estimated
+      ret.steerRatio = 15.6
+      tire_stiffness_factor = 1.0
 
     # TODO: get actual value, for now starting with reasonable value for
     # civic and scaling by mass and wheelbase
