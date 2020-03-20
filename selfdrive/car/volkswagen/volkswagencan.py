e0fd1bc6bf65864b6cfffa61562a37a0fc714cf0
Jason Young <jyoung8607@gmail.com>
2020-03-20 00:07:45 -0400

    Canonicalize the PQ HCA_1 message and signal naming

--- selfdrive/car/volkswagen/volkswagencan.py
+++ selfdrive/car/volkswagen/volkswagencan.py
@@ -63,16 +63,16 @@ def create_mqb_acc_buttons_control(packer, bus, buttonStatesToSend, CS, idx):
 
 def create_pq_steering_control(packer, bus, apply_steer, idx, lkas_enabled):
   values = {
-    "PQ_HCA_COUNTER": idx,
+    "HCA_Zaehler": idx,
     "LM_Offset": abs(apply_steer),
     "LM_OffSign": 1 if apply_steer < 0 else 0,
     "HCA_Status": 5 if (lkas_enabled and apply_steer != 0) else 3,
     "Vib_Freq": 16,
   }
 
-  dat = packer.make_can_msg("PQ_HCA", bus, values)[2]
-  values["PQ_HCA_CHECKSUM"] = dat[1] ^ dat[2] ^ dat[3] ^ dat[4]
-  return packer.make_can_msg("PQ_HCA", bus, values)
+  dat = packer.make_can_msg("HCA_1", bus, values)[2]
+  values["HCA_Checksumme"] = dat[1] ^ dat[2] ^ dat[3] ^ dat[4]
+  return packer.make_can_msg("HCA_1", bus, values)
 
 def create_pq_hud_control(packer, bus, hca_enabled, steering_pressed, hud_alert, leftLaneVisible, rightLaneVisible):
   if hca_enabled:
